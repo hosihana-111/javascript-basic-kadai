9c951ec5cd7ebf8c1f738e79bcffc2886cd99dd3
hosihana-111 <168593657+hosihana-111@users.noreply.github.com>
2024-06-23 06:41:43 +0900

    課題の提出

---
+++ kadai_007/calc.js
@@ -0,0 +1,15 @@
+for(let i = 1; i <= 15; i ++){
+    if( i %15 ===0){
+        console.log('3と5の倍数です')
+    }
+    else if(i % 5 ===0){
+        console.log('5の倍数です')
+    }
+    else if(i % 3 ===0){
+        console.log('3の倍数です')
+    }
+    else{
+        console.log(i)
+    }
+
+}
