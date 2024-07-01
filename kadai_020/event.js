36c93993551f25a571d72a211213e7781201c24a
hosihana-111 <168593657+hosihana-111@users.noreply.github.com>
2024-07-02 04:36:58 +0900

    課題の提出

---
+++ kadai_020/event.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById('btn');
+
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+       
+        const clickBtn = document.createElement('li');
+
+        clickBtn,textContent ='ボタンをクリックしました'
+
+        text.textContent    = "ボタンをクリックしました";
+
+  });
